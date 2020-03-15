5b4f1d75dbd37953e94bfa4fd63be99614042b9d
Konkord360 <Konkord720@gmail.com>
2020-03-15 22:34:03 +0100

    first commit

---
+++ src/main/java/com/koncor/mailReminder/SpringTestApplication.java
@@ -0,0 +1,37 @@
+package com.koncor.mailReminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@SpringBootApplication
+public class SpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestApplication.class, args);
+    }
+
+//    @Bean
+//    ApplicationRunner applicationRunner(UserRepository userRepository) {
+//        return args -> {
+//            User u = new User();
+//            u.setLogin("SpringTest");
+//            u.setPassword("SpringTest".toCharArray());
+//            u.setSalt("SpringTest".getBytes());
+//
+//			System.out.println(userRepository.save(u));
+//			System.out.println(userRepository.findByLogin("SpringTest"));
+//			System.out.println(userRepository.findByLogin("Konrad"));
+//        };
+//
+//    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        source.setCacheSeconds(3600); // Refresh cache once per hour.
+        return source;
+    }
+}
---
+++ src/main/java/com/koncor/mailReminder/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.koncor.mailReminder.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder((passwordEncoder()))
+                .usersByUsernameQuery(
+                        "select username, password, 'true' as enabled from users where username=?")
+                .authoritiesByUsernameQuery(
+                        "select user_login, role from user_roles where user_login=?");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/dashboard/**").access("hasRole('ROLE_USER')")
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login?error").defaultSuccessUrl("/dashboard")
+                .usernameParameter("username").passwordParameter("password")
+                .and()
+                .logout().logoutSuccessUrl("/login?logout")
+                .and()
+                .exceptionHandling().accessDeniedPage("/403")
+                .and()
+                .csrf();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
